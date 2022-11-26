f62b269218a3a8953498590b941687767e410c6a
Jamie Furlong <jamiefurlong16@gmail.com>
2022-11-26 14:31:53 +0000

    Forms & Flashes
    Created forms for Login and register
    Created templates for login and register.
    Dummy user object created in index route.
    
    Error flashes made using alerts from bootstrap.
    Login activates a flashed message confirming login request.

--- app/forms.py
+++ app/forms.py
@@ -2,11 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class login(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
 
-class register(FlaskForm):
-    firstName = StringField('First Name', validators=[DataRequired()])
-    lastName = StringField('Last Name', validators=[DataRequired()])
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Create Account')
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,39 @@
-from flask import render_template
+from flask import flash, redirect, render_template
 from app import app
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 @app.route('/landing')
 def landing():
-    
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title="Welcome")
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return 'Login Page'
+    form = LoginForm()
+
+    #Post Method
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Log In', form=form)
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"] )
 def register():
-    return 'Register Page'
+    form = RegisterForm()
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/mission')
 def mission():
