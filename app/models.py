89c16c4ad5a9de2b4ee9c18ce2bed819abd8b89f
Jamie Furlong <jamiefurlong16@gmail.com>
2022-11-26 16:04:08 +0000

    Password Hash
    Imported hashing methods from werkzeug.
    Created functions in User model for setting and checking a password.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +16,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {} {}>'.format(self.firstName, self.lastName)
+
+    # Sets the password on registration.
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    # Checks a password on login.
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
